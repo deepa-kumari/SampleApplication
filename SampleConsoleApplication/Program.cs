de990ffb2fd57a3e1f2386f63c00e51b52b6f34d
deepa-kumari <deeparawat2008@gmail.com>
2013-06-19 00:32:41 +0530

    String added.

--- SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string myString = "Hello Test123 456";
+            string myString = "Hello Test123 adsfsgffgh456";
             
         }
     }
