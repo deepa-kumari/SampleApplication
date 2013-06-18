00deef35256b0a0c979f46fe140c7c20d891366c
deepa-kumari <deeparawat2008@gmail.com>
2013-06-19 00:23:31 +0530

    String Change

--- SampleConsoleApplication/Program.cs
+++ SampleConsoleApplication/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string myString = "Hello Test";
+            string myString = "Hello Test123";
+            
         }
     }
 }
